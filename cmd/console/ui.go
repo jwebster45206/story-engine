608526446f0bde55c7969d733eec05ee9b00a3d5
Joe Webster <jwebster45206@gmail.com>
2026-04-16 08:03:26 -0400

    fix: plot directive output (#135)

--- cmd/console/ui.go
+++ cmd/console/ui.go
@@ -540,7 +540,9 @@ func (m *ConsoleUI) writeChatContent() {
 				content.WriteString(formattedMsg + "\n\n")
 			}
 		case "user":
-			userMsg := userStyle.Render(wordwrap.String(msg.Content, chatWidth-3))
+			displayContent := strings.ReplaceAll(msg.Content, "<plot_directive>", "")
+			displayContent = strings.ReplaceAll(displayContent, "</plot_directive>", "")
+			userMsg := userStyle.Render(wordwrap.String(strings.TrimSpace(displayContent), chatWidth-3))
 			content.WriteString(userMsg + "\n\n")
 		}
 	}
